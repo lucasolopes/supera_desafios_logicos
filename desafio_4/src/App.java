8f88a08432accf6ac071eff61651296f9a22a23e
lucasolopes <olopes.lucas567@gmail.com>
2022-12-16 18:43:33 -0300

    desafio 4 completo

--- desafio_4/src/App.java
+++ desafio_4/src/App.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    	Scanner input = new Scanner(System.in);
+    	int quantidade = input.nextInt();
+    	ArrayList<String> linha = new ArrayList<>();
+    	for(int c = 0; c<=quantidade;c++) {
+    		linha.add(input.nextLine());
+    	}
+    	
+    	linha.forEach(texto -> 
+    		{
+				String parte1 = new StringBuilder(texto.substring(0, texto.length()/2)).reverse().toString();
+				String parte2 = new StringBuilder(texto.substring(texto.length()/2)).reverse().toString();
+				String decifrado = parte1+parte2;
+				System.out.println(decifrado);
+			}
+    	);
+
+
     }
 }
