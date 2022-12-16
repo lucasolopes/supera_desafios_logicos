c88037643e0f5ad95f8a30da4425a8c0a191695d
lucasolopes <olopes.lucas567@gmail.com>
2022-12-16 17:59:03 -0300

    desafio 3 completo

--- desafio_3/src/App.java
+++ desafio_3/src/App.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) throws Exception {
+		Scanner input = new Scanner(System.in);
 
+        int tamanhoArray = input.nextInt();
+        int valorAlvo = input.nextInt();
 		
+        ArrayList<Integer> array = new ArrayList<>();
+        
+        for(int c= 0; c< tamanhoArray; c++) {
+        	int entradaArray = input.nextInt();
+        	array.add(entradaArray);
+        }
+        
+        Integer quantidadeIgualdadeAlgo = 0;
+        for (Integer primeiroValor : array) {
+			for (Integer segundoValor : array) {
+				if(primeiroValor - segundoValor == valorAlvo)
+					quantidadeIgualdadeAlgo++;
+			}
+		}
+        
+        System.out.println(quantidadeIgualdadeAlgo);
 		
 	}
 }
